af6fb1a6150dc517e0be5e4d1a899707d9f52e00
DanielTwito <Daniel.twito7@gmail.com>
2019-12-21 16:19:40 +0200

    addind a popup class addind a valdiation for location

--- backend.py
+++ backend.py
@@ -7,11 +7,36 @@ class Database():
         files = os.listdir("./")
         if "database.db" in files:
             self.conn = sqlite3.connect("database.db")
-            return
         else:
             data = pd.read_csv("BikeShare.csv")
             self.conn=sqlite3.connect("database.db")
             data.to_sql("BikesInfo_tbl",self.conn,if_exists="replace")
+        self.loclist=self.get_locations()
 
+    """
+    this function extract all the location from the start ans end location
+    and make a set from them
+    """
+    def get_locations(self):
+        cur = self.conn.cursor()
+        li1= cur.execute("""
+                        SELECT DISTINCT StartStationName  FROM BikesInfo_tbl
+                        """).fetchall()
+        li2= cur.execute("""
+                        SELECT DISTINCT EndStationName  FROM BikesInfo_tbl
+                        """).fetchall()
+        li1=[t[0].lower() for t in li1 ]
+        li2=[t[0].lower() for t in li2 ]
+        union =  list(set(li1) | set(li2))
+        return union
 
-d=Database()
+    """
+    check if a given location is exist inside of a distinct location list
+    that we build in the constructor
+    """
+    def is_location_exist(self,loc):
+        ans = loc.lower() in self.loclist
+        return ans
+
+    def recommand(self, numOfRec):
+        return ["test1","test2","test3"]
--- frontend.py
+++ frontend.py
@@ -5,7 +5,22 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
 from kivy.uix.popup import Popup
 from kivy.uix.widget import Widget
+from backend import Database
 
+
+class MyPopup(Popup):
+
+    def __init__(self, **kwargs):
+        grid = GridLayout(cols=1)
+        super().__init__(title="Input Error", content=grid,size_hint = (.4, .4))
+        self.msg = Label(halign="center", valign="middle")
+        btn = Button(text="close", size_hint=(1, 0.3))
+        btn.bind(on_press=lambda x: self.dismiss())
+        grid.add_widget(self.msg)
+        grid.add_widget(btn)
+
+    def set_msg(self,text):
+        self.msg.text=text
 
 
 class MyGrid(Widget):
@@ -13,35 +28,40 @@ class MyGrid(Widget):
     time=ObjectProperty(None)
     numOfRec=ObjectProperty(None)
 
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.database=Database()
+
     def submit_btn_handler(self):
         print("location: ",self.loc.text,"time: ",self.time.text,"Recommendation number: ",self.numOfRec.text)
-        is_valid = self.validate_input(self.loc.text,self.time.text,self.numOfRec.text)
-        if not is_valid:
+        valid_input = self.validate_input(self.loc.text,self.time.text,self.numOfRec.text)
+        if not valid_input:
             return
+        x=self.database.recommand(self.numOfRec)
+        print(x )
+
         self.loc.text=""
         self.time.text=""
         self.numOfRec.text=""
 
     def validate_input(self,location,d_time,rec_number):
-        msg = Label(halign="center",valign="middle")
-        btn = Button(text="close",size_hint=(1,0.3))
-        btn.bind(on_press=lambda x:popup.dismiss())
-        grid=GridLayout(cols=1)
-        grid.add_widget(msg)
-        grid.add_widget(btn)
-        popup = Popup(title="Input Error",content=grid)
-        popup.size_hint=(.4,.4)
-        #TODO - valdiation of the location
+        popup=MyPopup()
+        """validate if location exist in the DB"""
+        if not self.database.is_location_exist(location):
+            popup.set_msg("location cant be found")
+            popup.open()
+            return False
+        """check if duration time is an integer number"""
         try:
             int(d_time)
         except:
-            msg.text="time must be an integer number"
+            popup.set_msg("time must be an integer number")
             popup.open()
             return False
         try:
             int(rec_number)
         except:
-            msg.text = "number of recommendation\nmust be an integer number"
+            popup.set_msg( "number of recommendation\nmust be an integer number")
             popup.open()
             return False
         return True
